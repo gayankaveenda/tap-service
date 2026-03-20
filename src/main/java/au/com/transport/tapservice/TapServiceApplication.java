5a5aca29d2ecd1da7580d0c0567454e5d799c0d2
Gayan Kaveenda <gayankaveenda@gmail.com>
2026-03-21 10:42:58 +1100

    project setup, swagger, actuator endpoints,docker postgres setup for project start up

---
+++ src/main/java/au/com/transport/tapservice/TapServiceApplication.java
@@ -0,0 +1,13 @@
+package au.com.transport.tapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TapServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/au/com/transport/tapservice/TapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.transport.tapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
